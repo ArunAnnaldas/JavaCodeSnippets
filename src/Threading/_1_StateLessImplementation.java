cc450a958378918e1e5a386e3de959bd7902973d
ArunAnnaldas <cmeatarun1988@gmail.com>
2019-07-19 01:21:54 +0530

    Committing the files to illustrate Thread safe mechanism using Immutablility

---
+++ src/Threading/_1_StateLessImplementation.java
@@ -0,0 +1,29 @@
+package Threading;
+
+import java.math.BigInteger;
+
+public class _1_StateLessImplementation {
+
+	public static void main(String[] args) {
+		
+		testFactorial(5);
+	}
+	
+	/**
+	 * The testfactorial() method is a stateless deterministic function. 
+	 * Given a specific input, it always produces the same output.
+	 * The method neither relies on external state nor maintains state at all. 
+	 * Hence, it’s considered to be thread-safe and can be safely called by multiple threads at the same time.
+	 * 
+	 * @param number
+	 */
+	
+	public static void testFactorial(int number){
+		
+		BigInteger f = new BigInteger("1");
+		for (int i = 2; i <= number; i++) {
+			f = f.multiply(BigInteger.valueOf(i));
+		}
+		System.out.println(f);
+	}
+}
---
+++ src/Threading/_2_ImmutableImplementation.java
@@ -0,0 +1,19 @@
+package Threading;
+
+public class _2_ImmutableImplementation {
+
+	private final String message;
+
+	/**
+	 * If we need to share state between different threads, we can create
+	 * thread-safe classes by making them immutable. To put it simply, a class
+	 * instance is immutable when its internal state can’t be modified after it has
+	 * been constructed. The easiest way to create an immutable class in Java is by
+	 * declaring all the fields private and final and not providing setters:
+	 * 
+	 */
+	public _2_ImmutableImplementation(String message) {
+		this.message = message;
+	}
+
+}
