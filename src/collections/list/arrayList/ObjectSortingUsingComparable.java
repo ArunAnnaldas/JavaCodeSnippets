ed6a3f26952327070b3a5917d9b7e57e17fc8dd4
ArunAnnaldas <cmeatarun1988@gmail.com>
2019-07-21 00:31:20 +0530

    Object Sorting Using Comparable Interface Implementation

--- src/collections/list/arrayList/ObjectSortingUsingComparable.java
+++ src/collections/list/arrayList/ObjectSortingUsingComparable.java
@@ -64,7 +64,7 @@ class Student implements Comparable {
 	}
 }
 
-public class ObjectSorting {
+public class ObjectSortingUsingComparable {
 	public static void main(String args[]) {
 		ArrayList<Student> arraylist = new ArrayList<Student>();
 		arraylist.add(new Student(223, "Chaitanya", 26));
