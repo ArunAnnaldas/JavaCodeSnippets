0f4de40f69eb4c1ec223946218f95a8637d51889
ArunAnnaldas <cmeatarun1988@gmail.com>
2019-07-21 02:39:10 +0530

    Conversions to ArrayList

---
+++ src/collections/list/arrayList/conversions/LinkedListToArrayList.java
@@ -0,0 +1,22 @@
+package collections.list.arrayList.conversions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListToArrayList {
+	public static void main(String[] args) {
+		LinkedList<String> linkedlist = new LinkedList<String>();
+		linkedlist.add("Harry");
+		linkedlist.add("Jack");
+		linkedlist.add("Tim");
+		linkedlist.add("Rick");
+		linkedlist.add("Rock");
+
+		List<String> list = new ArrayList<String>(linkedlist);
+
+		for (String str : list) {
+			System.out.println(str);
+		}
+	}
+}
