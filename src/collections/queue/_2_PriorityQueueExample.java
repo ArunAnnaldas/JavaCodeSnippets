799d0c35aad47b96aa86fce094c6a242e34a25e0
ArunAnnaldas <cmeatarun1988@gmail.com>
2019-07-23 10:50:51 +0530

    adding queue documentation

--- src/collections/queue/_2_PriorityQueueExample.java
+++ src/collections/queue/_2_PriorityQueueExample.java
@@ -10,6 +10,17 @@ import java.util.PriorityQueue;
  * PriorityQueue, which serves the request based on the priority that we set
  * using Comparator.
  * 
+ * Strengths: Fast operations. All queue operations take O(1)O(1) time.
+ * =========
+ * 
+ * Uses: Breadth-first search uses a queue to keep track of the nodes to visit
+ * next. Printers use queues to manage jobs—jobs get printed in the order
+ * they're submitted. Web servers use queues to manage requests—page requests
+ * get fulfilled in the order they're received. Processes wait in the CPU
+ * scheduler's queue for their turn to run.
+ * 
+ * Worst Case :  space O(n) enqueue O(1) dequeue O(1) peek O(1)
+ * 
  * @author janar
  *
  */
@@ -37,13 +48,11 @@ public class _2_PriorityQueueExample {
 
 }
 
-class MyComparator implements Comparator<String>{
+class MyComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
-		return o1.length()-o2.length();
+		return o1.length() - o2.length();
 	}
-	
-	
-}
 
+}
